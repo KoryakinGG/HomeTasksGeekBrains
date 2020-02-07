bcd517b5f43d8d6c466c2a65e87d931c6e71997f
Gennadiy Koryakin <koryaking@mail.ru>
2020-02-07 23:36:16 +0300

    Домашняя работа № 1 (Исправлены ошибки в 4,5,8 примерах)

--- src/Java_1/Lesson_1/HomeWork_1.java
+++ src/Java_1/Lesson_1/HomeWork_1.java
@@ -1,65 +1,65 @@
 package Java_1.Lesson_1;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
 public class HomeWork_1 {
 
     public static void main(String[] args) {
         // создать переменные всех типов
-        byte b = 104;
-        short s = 4657;
+        byte b = 114;
+        short s = 4757;
         int i = 541198465;
-        long l = 5465215455444L;
+        long l = 546521455444L;
         char c = 'c';
         boolean bl = true;
-        float f = 23434.23434f;
-        double d = 234234234234.23423423434;
+        float f = 2344.23434f;
+        double d = 23423423234.23423423434;
         System.out.println(expression1((byte) 5, (short) 450, 80, 2.0));
-        System.out.println(expression2(5, 14));
-        System.out.println("Число " + positiveOrNegative(-55));
+        System.out.println(expression2(4, 14));
+        System.out.println("Число " +  positiveOrNegative(-55));
         System.out.println(negative(-6));
-        hello("Билли Боб  Торнтон");
-        System.out.println(isLeapYear(2020));
+        hello("Билли Боб Торнтон");
+        isLeapYear(2020);
     }
 
     //Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,где a, b, c, d – входные параметры этого метода;
     public static double expression1(byte a, short b, int c, double d) {
-        return  a * (b + (c / d));
+        return a * (b + (c / d));
     }
 
     /*Написать метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах от 10 до 20(включительно),
     если да – вернуть true, в противном случае – false;*/
     public static boolean expression2(int x, int y) {
         int z = x + y;
-        return (z>= 10 && z <= 20);
+        return (z >= 10 && z <= 20);
     }
 
     /*Написать метод, которому в качестве параметра передается целое число,
     метод должен напечатать в консоль положительное ли число передали, или отрицательное; Замечание: ноль считаем положительным числом.
      */
-    static boolean positiveOrNegative(int x) {
-
+    static boolean positiveOrNegative( int x) {
         return x >= 0;
     }
 
     //Написать метод, которому в качестве параметра передается целое число, метод должен вернуть true, если число отрицательное;
-    private static boolean negative(int x) {
-
+    private static boolean negative( int x) {
         return x < 0;
     }
 
-    //Написать метод, которому в качестве параметра передается строка, обозначающая имя,
-    // метод должен вывести в консоль сообщение «Привет, указанное_имя!»;
+    //Написать метод, которому в качестве параметра  передается строка, обозначающая имя,
+    // метод должен вывести в консоль сообщение « Привет, указанное_имя!»;
     private static void hello(String s) {
-        System.out.println("Привет, " + s + "!");
+        System.out.println(" Привет, " + s + "!");
     }
 
-    /*Написать метод, который определяет является ли год високосным, и выводит сообщение в консоль.
+    /*Написать метод, который определяет является  ли год високосным, и выводит сообщение в консоль.
     Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный. */
-    static String isLeapYear(int x) {
-        String s;
-
-        if ((x % 400 == 0) || (x % 4 == 0) && (x % 100 != 0))
-        {s = "Год високосный";}
-        else {s = "Год не викосный";}
-        return s;
+    static void isLeapYear(int x) {
+        if ((x % 400 == 0) || (x % 4 == 0) && (x % 100 != 0)) {
+            System.out.println("Год високосный");
+        } else {
+            System.out.println("Год не високосный");
+        }
     }
 }
