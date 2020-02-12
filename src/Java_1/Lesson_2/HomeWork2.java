d028ea091469be09afc8b91c75a498467e4deeea
Gennadiy Koryakin <koryaking@mail.ru>
2020-02-12 08:34:51 +0300

    Домашняя работа № 2 (Кирилл, исправил все основные замечания, но до 7 задачи сегодня не добрался. Подумаю чуть позже. Ломается мозг от твой замечаний, за это я тебе благодарен. =) )

--- src/Java_1/Lesson_2/HomeWork2.java
+++ src/Java_1/Lesson_2/HomeWork2.java
@@ -2,19 +2,23 @@ package Java_1.Lesson_2;
 
 import java.util.Arrays;
 
-public class HomeWork_2 {
+public class HomeWork2 {
     public static void main(String[] args) {
-        //Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+        //Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+        // С помощью цикла и условия заменить 0 на 1, 1 на 0;
         int[] expression1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         invertTheNumbers(expression1);
+        System.out.println(Arrays.toString(expression1));
 
         //Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
         int[] expression2 = new int[8];
         plusByThree(expression2);
+        System.out.println(Arrays.toString(expression2));
 
         //Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
         int[] expression3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         multiplyByTwo(expression3);
+        System.out.println(Arrays.toString(expression3));
 
         //Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
         // и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
@@ -23,23 +27,22 @@ public class HomeWork_2 {
 
         //Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
         int[] findNumbers = {-100, 54, 825, 96369, -65778, 0, 145, 4821, -5541};
-        Arrays.sort(findNumbers);
-        System.out.println("Минимальный элемент = " + findNumbers[0] + "\n" + "Максимальный элемент = " + findNumbers[findNumbers.length - 1]);
+        System.out.println("Максимальный элемент: " + maxArray(findNumbers));
+        System.out.println("Минимальный эдемент: " + minArray(findNumbers));
 
         /*Написать метод, в который передается не пустой одномерный целочисленный массив,
         метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
         Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true, checkBalance([1, 1, 1, || 2, 1]) → true,
         граница показана символами ||, эти символы в массив не входят.*/
-        int[] expression6 = {2, 2, 2, 1, 2, 2, 10, 5};  //false, на экран не выводится
+        int[] expression6 = {2, 2, 2, 1, 2, 2, 10, 5};  //false
         int[] expression7 = {-10, 40, 5, -10, 40, 5};   //true
-        checkBalance(expression6);
-        checkBalance(expression7);
+        System.out.println(checkBalance(expression6));
+        System.out.println(checkBalance(expression7));
 
         /*Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным),
         при этом метод должен сместить все элементы массива на n позиций.
         Для усложнения задачи нельзя пользоваться вспомогательными массивами.*/
         int[] expression8 = {1, 2, 3, 4, 5, 6, 7};
-        offsetArray(expression8, -3);
     }
 
     public static void invertTheNumbers(int[] array) {
@@ -50,23 +53,12 @@ public class HomeWork_2 {
                 array[i] = 1;
             }
         }
-        System.out.print("Результат 1 задания: ");
-        for (int showMeArray1 : array) {
-            System.out.print(showMeArray1 + " ");
-        }
-        System.out.println();
     }
 
     public static void plusByThree(int[] array) {
-
-        for (int i = 1; i < array.length; i++) {
-            array[i] = array[i - 1] + 3;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i * 3;
         }
-        System.out.print("Результат 2 задания: ");
-        for (int showMeArray2 : array) {
-            System.out.print(showMeArray2 + " ");
-        }
-        System.out.println();
     }
 
     public static void multiplyByTwo(int[] array) {
@@ -75,30 +67,40 @@ public class HomeWork_2 {
                 array[i] *= 2;
             }
         }
-        System.out.print("Результат 3 задания: ");
-        for (int showMeArray3 : array) {
-            System.out.print(showMeArray3 + " ");
-        }
-        System.out.println();
     }
 
     public static void diagonally(int[][] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (j == i) {
-                    array[i][j] = 1;
-                } else if (i + j == array.length - 1) {
-                    array[i][j] = 1;
-                } else {
-                    array[i][j] = 0;
-                }
+            array[i][i] = 1;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++)
                 System.out.print(array[i][j] + " ");
-            }
             System.out.println();
         }
     }
 
-    public static void checkBalance(int[] array) {
+    public static int maxArray(int[] array) {
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static int minArray(int[] array) {
+        int min = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static boolean checkBalance(int[] array) {
         int summ = 0;
         for (int value : array) {
             summ += value;
@@ -108,37 +110,9 @@ public class HomeWork_2 {
         for (int i = 0; i < array.length - 1; i++) {
             count += array[i];
             if (count == halfSumm) {
-                System.out.println("true");
+                return true;
             }
         }
-    }
-    /*Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным),
-        при этом метод должен сместить все элементы массива на n позиций.
-        Для усложнения задачи нельзя пользоваться вспомогательными массивами.*/
-
-    public static int[] offsetArray(int[] array, int n) {
-        if (n != 0) {
-            if (n > 0) {
-                for (int i = 0; i < n; i++) {
-                    int x = array[0];
-                    array[0] = array[array.length - 1];
-                    for (int j = 1; j < array.length - 1; j++) {
-                        array[array.length - j] = array[array.length - j - 1];
-                    }
-                    array[1] = x;
-                }
-            } else {
-                for (int i = 0; i > n; n--) {
-                    int x = array[array.length - 1];
-                    array[array.length - 1] = array[0];
-                    for (int j = 1; j < array.length - 1; j++) {
-                        array[j - 1] = array[j];
-                    }
-                    array[array.length - 2] = x;
-                }
-                System.out.println(Arrays.toString(array));
-            }
-        }
-        return array;
+        return false;
     }
 }
