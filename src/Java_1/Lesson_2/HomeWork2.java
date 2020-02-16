93051abdf4d5859f854890005d6072c827f0ebb8
Gennadiy Koryakin <koryaking@mail.ru>
2020-02-16 08:02:08 +0300

    Домашняя работа № 2

--- src/Java_1/Lesson_2/HomeWork2.java
+++ src/Java_1/Lesson_2/HomeWork2.java
@@ -71,19 +71,18 @@ public class HomeWork2 {
         }
     }
 
-    public static void diagonally(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            array[i][i] = 1;
+    public static int [][] diagonally(int[][] array) {
+        int n = 0;
+        int[][] arr = new int [n][n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i][i] = 1;
+            arr[i][n-1-i] = 1;
         }
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++)
-                System.out.print(array[i][j] + " ");
-            System.out.println();
-        }
+        return arr;
     }
 
     public static int maxArray(int[] array) {
-        int max = 0;
+        int max = array[0];
         for (int i = 0; i < array.length; i++) {
             if (array[i] > max) {
                 max = array[i];
@@ -93,7 +92,7 @@ public class HomeWork2 {
     }
 
     public static int minArray(int[] array) {
-        int min = 0;
+        int min = array[0];
         for (int i = 0; i < array.length; i++) {
             if (array[i] < min) {
                 min = array[i];
