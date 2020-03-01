6b3ceeb56d9c89422c5bfc1ebc5fc97b323d49e6
Gennadiy Koryakin <koryaking@mail.ru>
2020-03-01 05:05:54 +0300

    Домашняя работа № 6

---
+++ src/Java1/Lesson6/Cat.java
@@ -0,0 +1,25 @@
+package Java1.Lesson6;
+
+import com.sun.javaws.IconUtil;
+
+public class Cat extends Animal {
+//    public Cat(String name) {
+//        super(name);
+//    }
+
+    @Override
+    public void run(int distance) {
+        if (distance < 200) {
+            super.run(distance);
+        } else {
+            System.out.format("%s устал(а) и отдыхает\n", getName());
+        }
+    }
+
+    @Override
+    public void swim(int distance) {
+        if (distance > 0) {
+            System.out.format("%s не умеет плавать\n", getName());
+        }
+    }
+}
