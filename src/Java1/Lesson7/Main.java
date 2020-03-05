d7f44a2fa160b057f3636da6d7671fc77b24df52
Gennadiy Koryakin <koryaking@mail.ru>
2020-03-05 14:55:39 +0300

    Домашняя работа № 7

--- src/Java1/Lesson7/Main.java
+++ src/Java1/Lesson7/Main.java
@@ -1,24 +1,4 @@
 package Java1.Lesson7;
 
 public class Main {
-    public static void main(String[] args) {
-        Plate plate = new Plate(100);
-        Cat[] cats = new Cat[5];
-        cats[0] = new Cat("mr.Catty", 20);
-        cats[1] = new Cat("Terminator", 40);
-        cats[2] = new Cat("Pirate", 30);
-        cats[3] = new Cat("Eagle", 10);
-        cats[4] = new Cat("Boss", 5);
-
-        for (Cat cat : cats) {
-            cat.infoHungry();
-            cat.eat(plate);
-            cat.infoHungry();
-            plate.info();
-            System.out.println();
-        }
-        plate.putTheFood();
-        plate.info();
-
-    }
 }
