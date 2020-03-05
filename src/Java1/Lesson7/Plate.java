77c1377ff2c3c6cd71bfc0a8e707c0182e0dc68a
Gennadiy Koryakin <koryaking@mail.ru>
2020-03-05 15:01:01 +0300

    Домашняя работа № 7

--- src/Java1/Lesson7/Plate.java
+++ src/Java1/Lesson7/Plate.java
@@ -1,4 +1,31 @@
 package Java1.Lesson7;
 
+import java.util.Scanner;
+
 public class Plate {
+    Scanner scanner = new Scanner((System.in));
+    private int food;
+
+    protected Plate(int food) {
+        this.food = food;
+    }
+
+    protected void info() {
+        System.out.println("Food in plate: " + food);
+    }
+
+    protected boolean decreaseFood(int appetite) {
+        if (appetite > food) {
+            return false;
+        }
+
+        food -= appetite;
+        return true;
+    }
+
+    protected void putTheFood() {
+        int put = scanner.nextInt();
+        this.food += put;
+    }
+
 }
