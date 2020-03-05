d401e5297f5dcafb4d6c1ddaa4ff7ceb3e8b4825
Gennadiy Koryakin <koryaking@mail.ru>
2020-03-05 15:48:06 +0300

    Домашняя работа № 7

--- src/Java1/Lesson7/Cat.java
+++ src/Java1/Lesson7/Cat.java
@@ -1,25 +1,4 @@
 package Java1.Lesson7;
 
 public class Cat {
-    private String name;
-    private int appetite;
-    private boolean hungry;
-
-    protected Cat(String name, int appetite) {
-        this.name = name;
-        this.appetite = appetite;
-        this.hungry = true;
-    }
-
-    protected void eat(Plate plate) {
-        System.out.println("Cat " + name + " eating...");
-        if (hungry && plate.decreaseFood(appetite)) {
-            hungry = false;
-        }
-    }
-
-    protected void infoHungry() {
-        String isHungry = hungry ? "hungry" : "not hungry";
-        System.out.println(name + ": " + isHungry);
-    }
 }
