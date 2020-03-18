c4c620175a81ecf2f9aef2cca1b48bad79d0ac11
Gennadiy Koryakin <koryaking@mail.ru>
2020-03-19 01:54:49 +0300

    Домашка 5 (готово)

--- src/Java2/Lesson5/threadRunningTime.java
+++ src/Java2/Lesson5/threadRunningTime.java
@@ -1,4 +1,22 @@
 package Java2.Lesson5;
 
-public class threadRunningTime {
+public class threadRunningTime extends Thread {
+
+    private float[] multiple;
+
+    public threadRunningTime(String name, float[] multiple) {
+        super(name);
+        this.multiple = multiple;
+        start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < multiple.length; i++) {
+            multiple[i] = 1;
+        }
+        for (int i = 0; i < multiple.length; i++) {
+            multiple[i] = (float) (multiple[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
 }
